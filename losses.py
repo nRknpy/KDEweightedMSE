0dff7e3ca58ece57dd9e1f1deedb3baa06ebdf96
nRknpy <ofuro.jabu.jabu@gmail.com>
2023-05-02 20:08:15 +0900

    add scipy version

--- losses.py
+++ losses.py
@@ -3,10 +3,12 @@ import torch.nn as nn
 from torch.utils.data import DataLoader
 import numpy as np
 from sklearn.neighbors import KernelDensity
+from scipy.stats import gaussian_kde
 
 
-class KDEWeightedMSE(nn.Module):
+class KDEWeightedMSESl(nn.Module):
     def __init__(self, dataset, band_width, device, mode='divide', standardize=False, eps=1e-6):
+        super(KDEWeightedMSESl, self).__init__()
         assert mode in ('divide', 'onemin')
         self.band_width = band_width
         self.mode = mode
@@ -37,3 +39,30 @@ class KDEWeightedMSE(nn.Module):
         else:
             loss = torch.mean(torch.dot(1 - f, ((pred - target) ** 2).sum(1)))
         return loss
+
+class KDEWeightedMSESc(nn.Module):
+    def __init__(self, data, band_width, device, mode='divide', standardize=False, eps=1e-6):
+        super(KDEWeightedMSESc, self).__init__()
+        assert mode in ('divide', 'onemin')
+        self.mode = mode
+        self.band_width = band_width
+        self.eps = eps
+        self.device = device
+        self.kernel = self._kernel(data)
+        self.standardize = standardize
+        if self.standardize:
+            self.f_max = torch.max(torch.tensor(np.array(data).T))
+    
+    def forward(self, pred, target):
+        f = torch.tensor(self.kernel(target.cpu().T))
+        if self.standardize:
+            f = f / self.f_max
+        if self.mode == 'divide':
+            loss = torch.mean(torch.dot(1 / (f + self.eps).float().to(self.device), ((pred - target) ** 2).sum(1)))
+        else:
+            loss = torch.mean(torch.dot(1 - f, ((pred - target) ** 2).sum(1)))
+        return loss
+    
+    def _kernel(self, data):
+        data = np.array(data).T
+        return gaussian_kde(data, bw_method=self.band_width)
